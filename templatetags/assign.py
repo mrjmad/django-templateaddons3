f4812bf9b730e16ff5b027bccdc21e58c6398ae9
Benoit Bryon <bbryon@interfaceip.fr>
2008-08-13 09:40:42 +0200

    Added assign templatetag.

---
+++ templatetags/assign.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding= utf-8
+from django import template
+from templateutils.utils import decode_tag_arguments, parse_tag_argument
+
+
+register = template.Library()
+
+
+class AssignNode(template.Node):
+    def __init__(self, nodelist, arguments):
+        self.nodelist = nodelist
+        self.name = arguments['name']
+    
+    def render(self, context):
+        name = parse_tag_argument(self.name, context)
+        context[name] = self.nodelist.render(context)
+        return ''
+
+
+@register.tag
+def assign(parser, token):
+    default_arguments = {}
+    default_arguments['name'] = '"assign"'
+    arguments = decode_tag_arguments(token, default_arguments)
+    
+    nodelist = parser.parse(('endassign',))
+    parser.delete_first_token()
+    return AssignNode(nodelist, arguments)
